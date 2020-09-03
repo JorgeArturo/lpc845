44f6e9b6b29d42a1390d66f723ab7793043237f2
JorgeArturo <ing.jorge.mecatronico@gmail.com>
2020-09-03 18:55:06 -0500

    Tutorial Esp8266

--- Tutorial_WebServer__1/source/Tutorial_WebServer__1.c
+++ Tutorial_WebServer__1/source/Tutorial_WebServer__1.c
@@ -42,7 +42,7 @@
 #include <string.h>
 /* TODO: insert other include files here. */
 
-const char wp[] = {"<!DOCTYPE html>  <html>  <head>  <style>  table {    font-family: arial, sans-serif;    border-collapse: collapse;    width: 100%;  }    td, th {    border: 1px solid #dddddd;    text-align: left;    padding: 8px;  }    tr:nth-child(even) {    background-color: #dddddd;  }  </style>  </head>  <body>    <h2>ESP8266 Tutorial</h2>    <table>    <tr>      <th>Temperatura</th>      <th>Grados</th>    </tr>    <tr>      <td>Ambiente</td>      <td>27°C</td>    </tr>    </table>    </body>  </html>\0"};
+const char wp[] = {"<!DOCTYPE html> <html> <head> <title>ESP8266 WebServer</title> </head> <center><h2>Tutorial Simple para controlar una salida</h2></center> <form> LED0: <button name=\"LED\" value=\"ON0\" type=\"submit\">LED ON</button> <button name=\"LED\" value=\"OFF0\" type=\"submit\">LED OFF</button><br><br> </form> </html>\0"};
 
 
 unsigned int wp_len;
@@ -72,19 +72,20 @@ void USART1_USART_IRQHANDLER(void) {
   #endif
 }
 
-int8_t ESP8266_Response(char* resp,char* s){
+int8_t ESP8266_Response(char* resp,char* s,int st){
 
 	char* token;
 
 	flagrx = 0;
 	while(!flagrx);
 	flagrx = 0;
+	uint32_t to = 1000000;
 
 	token = strtok(resp,"\r\n");
 
-	while(token != NULL){
-		PRINTF("--> %s\r\n\0",token);
-		if(strcmp(token,s) == 0)
+	while((token != NULL) && to-->0){
+		//PRINTF("--> %s\r\n\0",token);
+		if(strncmp(token,s,st) == 0)
 			return 1;
 
 		token = strtok(NULL,"\r\n");
@@ -109,11 +110,11 @@ int8_t ESP8266_Init(char* SSID,char* pwd){
 
 	flagrx = 0;
 	USART_WriteBlocking(USART1_PERIPHERAL,"AT\r\n",4);
-    if(!ESP8266_Response(&r,"OK"))
+    if(!ESP8266_Response(&r,"OK",2))
     	return -1;
 
 	USART_WriteBlocking(USART1_PERIPHERAL,"AT+CWMODE_CUR=1\r\n",17);
-    if(!ESP8266_Response(&r,"OK"))
+    if(!ESP8266_Response(&r,"OK",2))
     	return -2;
 
     strcpy(con,"AT+CWJAP_CUR=\"");
@@ -128,29 +129,30 @@ int8_t ESP8266_Init(char* SSID,char* pwd){
 
     do{
     	USART_WriteBlocking(USART1_PERIPHERAL,"AT+CIPSTATUS\r\n",14);
-    }while(ESP8266_Response(&r,"busy p..."));
+    }while(ESP8266_Response(&r,"busy p...",9));
 
     //for(uint32_t t=5000000;t>0;t--);
     //Multi-Connections
 	USART_WriteBlocking(USART1_PERIPHERAL,"AT+CIPMUX=1\r\n",13);
-    if(!ESP8266_Response(&r,"OK"))
+    if(!ESP8266_Response(&r,"OK",2))
     	return -4;
 
     //Server Port default 80
 	USART_WriteBlocking(USART1_PERIPHERAL,"AT+CIPSERVER=1,80\r\n",19);
-    if(!ESP8266_Response(&r,"OK"))
+    if(!ESP8266_Response(&r,"OK",2))
     	return -5;
 
     //Timeout Client to 30 s
 
 	USART_WriteBlocking(USART1_PERIPHERAL,"AT+CIPSTO=30\r\n",14);
-    if(!ESP8266_Response(&r,"OK"))
+    if(!ESP8266_Response(&r,"OK",2))
     	return -6;
 
 
     return 1;
 
 }
+
 
 void ESP8266_WebServer(void){
 
@@ -159,22 +161,38 @@ void ESP8266_WebServer(void){
 	unsigned int con_len;
 	char* token;
 	int _n = -1;
+	char _dr[1];
+
+	//USER VARIABLES
+
+	char* ret;
+
+
+	//USER VARIABLES END
 
 	wp_len = sizeof(wp);
+
+	//Accept Connection
 
 	flagrx = 0;
 	while(!flagrx);
 	flagrx = 0;
 
+	DisableIRQ(USART1_USART_IRQN);
+	USART_ClearStatusFlags(USART1_PERIPHERAL,kUSART_RxReady);
+
 	token = strtok(r,",\r\n");
 
 	while(token != NULL){
-		if(strcmp(token,"+IPD")==0)
+		if(strncmp(token,"+IPD",4) == 0)
 			break;
 
 		token = strtok(NULL,",\r\n");
 
 	}
+
+	EnableIRQ(USART1_USART_IRQN);
+	USART_ClearStatusFlags(USART1_PERIPHERAL,kUSART_RxReady);
 
 	if(strcmp(token,"+IPD")==0){
 
@@ -182,11 +200,29 @@ void ESP8266_WebServer(void){
 
 		_n = atoi(token);
 
+		strncpy(_dr,token,1);
+
+		//USER CODE
+
+		token = strtok(NULL,",\r\n");
+
+		ret = strstr(token,"/?LED=");
+
+		if(strncmp(ret,"/?LED=ON0",9) == 0)
+			LED_GREEN_ON();
+		if(strncmp(ret,"/?LED=OFF0",10) == 0)
+			LED_GREEN_OFF();
+
+
+		//END CODE
+
+
+
 		if(_n >= 0 && _n<=5){
 
 			sprintf(clen,"%u",wp_len);
 			strcpy(con,"AT+CIPSEND=");
-			strcat(con,token);
+			strcat(con,_dr);
 			strcat(con,",");
 			strcat(con,clen);
 			strcat(con,"\r\n\0");
@@ -194,16 +230,37 @@ void ESP8266_WebServer(void){
 			con_len = strlen(con);
 
 			USART_WriteBlocking(USART1_PERIPHERAL,&con,con_len);
-			for(uint32_t t=1000000;t>0;t--);
 
-			USART_WriteBlocking(USART1_PERIPHERAL,&wp,wp_len);
-			for(uint32_t t=1000000;t>0;t--);
+			if(ESP8266_Response(&r,">",1) == 1){
 
-		}
+				USART_WriteBlocking(USART1_PERIPHERAL,&wp,wp_len);
+
+				if(ESP8266_Response(&r,"SEND OK",7) == 1){
+
+
+					strcpy(con,"AT+CIPCLOSE=");
+					strcat(con,_dr);
+					strcat(con,"\r\n\0");
+
+					USART_WriteBlocking(USART1_PERIPHERAL,&con,15);
+
+
+				}
+
+			}else
+				USART_WriteBlocking(USART1_PERIPHERAL,"AT+CIPCLOSE=5\r\n",15);
+
+		}else
+			USART_WriteBlocking(USART1_PERIPHERAL,"AT+CIPCLOSE=5\r\n",15);
+
+	}else{
+
+		USART_WriteBlocking(USART1_PERIPHERAL,"AT+CIPCLOSE=5\r\n",15);
 
 	}
 
-	USART_WriteBlocking(USART1_PERIPHERAL,"AT+CIPCLOSE=5\r\n",15);
+
+	for(uint16_t q = 0; q < sizeof(r);q++) r[q] = 0;
 }
 
 /*
@@ -220,6 +277,9 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
+
+    LED_GREEN_INIT(true);
+
     PRINTF("Esp8266 WEBserver\n");
 
     if(ESP8266_Init("TP-LINK_7456", "67320004") == 1)
@@ -233,6 +293,8 @@ int main(void) {
     	while(1);
 
     }
+
+    for(uint16_t q = 0; q < sizeof(r);q++) r[q] = 0;
 
     /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
